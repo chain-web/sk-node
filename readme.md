1423f6c88b0fbb34ef3e43f5bb0c9804a8b0659f
chengcheng <chengcheng@qcraft.ai>
2022-04-09 21:19:34 +0800

    some thing of sk-chain run at nodejs,but cannot run

---
+++ readme.md
@@ -0,0 +1,8 @@
+## sk-chain run at node
+
+### 开发
+
+```bash
+npm i
+ts-node ./scripts/node1.ts
+```
---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import typescript from 'rollup-plugin-typescript2';
+// import { terser } from 'rollup-plugin-terser';
+// import rollupReplace from 'rollup-plugin-replace';
+// import fileSize from 'rollup-plugin-filesize';
+
+const createTsPlugin = ({ declaration = true, target } = {}) =>
+  typescript({
+    clean: true,
+    tsconfig: "tsconfig.json",
+    tsconfigOverride: {
+      compilerOptions: {
+        declaration,
+        ...(target && { target }),
+      },
+    },
+  });
+
+const createNpmConfig = ({ input, output }) => ({
+  input,
+  output,
+  preserveModules: true,
+  plugins: [createTsPlugin()],
+});
+
+
+
+export default [
+  createNpmConfig({
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: 'dist',
+        format: 'cjs',
+      },
+    ],
+  }),
+];
