e4ed3e5c46bde9b0414b9b5cfeebb2943180492b
chengcheng <chengcheng@qcraft.ai>
2022-07-06 16:49:37 +0800

    some of test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { createNode,DidJson } from 'sk-chain';
+import { createNode, DidJson } from 'sk-chain';
 import { resolve } from 'path';
 import { homedir } from 'os';
 import BigNumber from 'bignumber.js';
+import { accounts } from './accounts';
 const account: DidJson = {
   id: '12D3KooWL8qb3L8nKPjDtQmJU8jge5Qspsn6YLSBei9MsbTjJDr8',
   privKey:
@@ -11,16 +12,16 @@ const account: DidJson = {
 const run = async () => {
   const node = await createNode({
     networkid: 'testnet',
-    account,
+    account: accounts[2],
     storePath: { main: resolve(homedir(), './.skdb') },
   });
-  setTimeout(() => {
-    node.transaction({
-      amount: new BigNumber(10),
-      recipient: '12D3KooWL1NF6fdTJ9cucEuwvuX8V8KtpJZZnUE4umdLBuK15eUZ',
-      payload: '',
-    });
-  }, 30000);
+  // setTimeout(() => {
+  //   node.transaction({
+  //     amount: new BigNumber(10),
+  //     recipient: '12D3KooWL1NF6fdTJ9cucEuwvuX8V8KtpJZZnUE4umdLBuK15eUZ',
+  //     payload: '',
+  //   });
+  // }, 30000);
 };
 
 run();
